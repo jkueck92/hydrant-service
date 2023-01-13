6ec49d013ceac9e94ccae7f6373808828215c261
jkueck <jkueck@eos-uptrade.de>
2023-01-13 07:45:00 +0100

    first commit

---
+++ src/main/java/de/jkueck/service/HydrantServiceApplication.java
@@ -0,0 +1,13 @@
+package de.jkueck.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrantServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/jkueck/service/HydrantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jkueck.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
